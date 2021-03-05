b57ae1a483d7f7da1c6f99e0bff629706a12592d
Rodrigo Presa <rodrigo_presa@outlook.com>
2021-03-05 14:43:04 -0300

    Refatorado cod. de verificação horiz.; msg. de empate; corrigido segundo clique na mesma célula; verif. de nomes iguais.

--- jogo.js
+++ jogo.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     var rodada = 1;
     var matriz_jogo = Array(3);
-
+    //Monta o array multidimensional que determina as posições no jogo
     matriz_jogo['a'] = Array(3);
     matriz_jogo['b'] = Array(3);
     matriz_jogo['c'] = Array(3);
@@ -27,9 +27,16 @@ $(document).ready(function(){
             alert('Favor digitar um nome válido para o jogador 2!');
             return false;
         }
-        //printa os nomes abaixo das respectivas imagens na palco_jogo
-        $('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val());
-        $('#nome_jogador_2').html($('#entrada_apelido_jogador_2').val());
+        if($('#entrada_apelido_jogador_1').val() == $('#entrada_apelido_jogador_2').val()){
+            //Se os nomes forem iguais, acrescenta '1' após o apelido do jogador 1 e '2' após o apelido do jogador 2
+            $('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val() + ' 1');
+            $('#nome_jogador_2').html($('#entrada_apelido_jogador_2').val() + ' 2');
+        }
+        else{
+            //Caso os nomes sejam diferentes, printa os nomes abaixo das respectivas imagens na palco_jogo
+            $('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val());
+            $('#nome_jogador_2').html($('#entrada_apelido_jogador_2').val());
+        }      
         //alterna a visualização das divs
         $('#pagina_inicial').hide();
         $('#palco_jogo').show();
@@ -37,7 +44,7 @@ $(document).ready(function(){
     //Recupera atraves do clique o id da posição clicada no jogo
     $('.jogada').click(function(){
         var id_posicao_clicada = this.id;
-        jogada(id_posicao_clicada)
+        jogada(id_posicao_clicada);
     });
 
     function jogada(id){
@@ -54,33 +61,20 @@ $(document).ready(function(){
         rodada++;
         //Insere a imagem referente ao jogador que clicou na posição
         $('#'+id).css('background-image', icone);
-
+        //Desabilita a opção de clicar na mesma posição novamente
+        $('#'+id).off();
         //recupera o valor da linha e da colula, removendo o separador do id da posição clicada
         var linha_coluna = id.split('-');
         matriz_jogo[linha_coluna[0]][linha_coluna[1]] = ponto;
 
-        verifica_combinacao();
+        verifica_combinacao(linha_coluna[0]);
     }
 
-    function verifica_combinacao(){
-        //VERIFICACAO NA HORIZONTAL
-
-        //Verifica pontos na horizontal, linha 'a'
+    function verifica_combinacao(linha){
+        //Verifica pontos na horizontal
         var pontos = 0;
         for (var h = 1; h <=3; h++) {
-            pontos = pontos + matriz_jogo['a'][h];            
-            ganhador(pontos);
-        }
-        //Verifica pontos na horizontal, linha 'b'
-        pontos = 0;
-        for (var h = 1; h <=3; h++) {
-            pontos = pontos + matriz_jogo['b'][h];            
-            ganhador(pontos);
-        }
-        //Verifica pontos na horizontal, linha 'c'
-        pontos = 0;
-        for (var h = 1; h <=3; h++) {
-            pontos = pontos + matriz_jogo['c'][h];            
+            pontos = pontos + matriz_jogo[linha][h];
             ganhador(pontos);
         }
         //Verifica pontos na vertical
@@ -91,7 +85,6 @@ $(document).ready(function(){
             pontos += matriz_jogo['c'][v];
             ganhador(pontos);
         }
-
         //Verifica pontos na diagonal
         pontos = 0;
         pontos += matriz_jogo['a'][1] + matriz_jogo['b'][2] + matriz_jogo['c'][3];
@@ -99,6 +92,8 @@ $(document).ready(function(){
         pontos = 0;
         pontos += matriz_jogo['a'][3] + matriz_jogo['b'][2] + matriz_jogo['c'][1];
         ganhador(pontos);
+
+        empate(pontos);
     }
 
     //Função que verifica qual jogador alcançou a pontuação necessária para vencer o jogo
@@ -114,4 +109,16 @@ $(document).ready(function(){
             $('.jogada').off();
         }
     }
+    //Função que verifica se deu empate
+    function empate(pontos){
+        if(rodada == 10 && pontos != 3){
+            alert('Deu velha!');
+            $('.jogada').off();
+        }
+    }
 });
+
+//Refatorada a lógica de verificação na horizontal
+//Criada uma função para verificar se o jogo terminou em empate
+//Desabilita a opção de clicar na mesma posição novamente
+//Criada uma verificação que altera os nomes dos jogadores caso sejam iguais
